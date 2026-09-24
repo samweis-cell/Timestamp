ca4a5d3a281b45c7b3e16a6f5731b12d93169e7a
samweis-cell <samuelnikweisburd@gmail.com>
2026-09-24 20:03:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,36 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('timestamp-v1').then((cache) => {
-      return cache.addAll(['./', './index.html', './manifest.json']);
+const CACHE_NAME = 'timestamp-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
